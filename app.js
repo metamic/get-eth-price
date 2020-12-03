3fe94155b2af96b3b386ace9d448bffeb81864e1
metamic <meta.microcode@gmail.com>
2020-12-04 08:49:03 +0900

    change logic

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ const port = process.env.EA_PORT || 9090
 
 app.use(bodyParser.json())
 
-app.post('/', (req, res) => {
-  console.log('POST Data: ', req.body)
-  createRequest(req.body, (status, result) => {
-    console.log('Result: ', result)
-    res.status(status).json(result)
-  })
+app.get('/', (req, res) => {
+    createRequest(req.query, (status, result) => {
+        console.log(result);
+        res.status(status).json(result);
+    })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
